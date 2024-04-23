d7a0d5cc2308ff73dbbf2bd05832c42081fed045
sofi bellatti <sofiabellatti@gmail.com>
2024-04-23 00:36:09 -0300

    iniciamos pantalla de productos (nav, banner, segundo banner con botones)

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('.slider').slick({
+          infinite: true,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+          nextArrow: '<button type="button" class="slick-next">Next</button>'
+      });
+    });
+    $('.slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+    
+    
+    ]
+    });
+    
